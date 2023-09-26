8164cc5f0cdd5a82368123340992ccebf1d66310
jmoggridge <ja.moggridge@gmail.com>
2023-09-26 15:16:23 -0700

    replace write_rds to data with usethis::use_data

--- scripts/script.R
+++ scripts/script.R
@@ -1,19 +1,20 @@
-# do scraping for all currently-active family doctors 
+# do scraping for all currently-active family doctors
 
 scraper <- modules::use('R/scraper.R')
 driver <- scraper$start_selenium_firefox()
 
-urls <- scraper$fetch_all_doctors_urls(
-  remote = driver$client, 
-  include_inactive = F, 
+active_family_doctor_urls <- scraper$fetch_all_doctors_urls(
+  remote = driver$client,
+  include_inactive = F,
   family_only = T
 )
+usethis::use_data(active_family_doctor_urls, overwrite = T)
 beepr::beep(1)
 
-doctors <- scraper$fetch_all_doctors_data(driver$client, urls)
+active_family_doctors <- scraper$fetch_all_doctors_data(driver$client, urls)
 filename <- stringr::str_glue('data/cpso_active_family_doctors_{Sys.Date()}.rds')
-readr::write_rds(doctors, filename)
+
+usethis::use_data(active_family_doctors, overwrite = T)
 driver$server$stop()
 beepr::beep(2)
 
-readr::write_rds(urls, 'data/urls.rds')
