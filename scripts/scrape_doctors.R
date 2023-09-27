243d0fc293d32e714890e0fdeac04f451f871457
jmoggridge <ja.moggridge@gmail.com>
2023-09-26 21:02:35 -0700

    crawler/scraper process finished and returned ~17,988 currently active family doctors

--- scripts/scrape_doctors.R
+++ scripts/scrape_doctors.R
@@ -6,3 +6,12 @@ doctors <- scraper$fetch_all_doctors_data(driver$client, urls)
 readr::write_rds(doctors, 'data/cpso_active_family_doctors.rds')
 driver$server$stop()
 beepr::beep(2)
+
+doctors |> 
+  tibble::glimpse()
+
+
+urls |> tibble::glimpse()
+
+doctors |> 
+  dplyr::filter(is.na(name) | is.na(gender))
